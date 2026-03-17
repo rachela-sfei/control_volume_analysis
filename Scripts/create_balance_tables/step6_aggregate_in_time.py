fixed bug to get multi year seasonal averages working again

Scripts/create_balance_tables/step6_aggregate_in_time.py
@@ -227,10 +227,10 @@ for balance_table_fn in table_list:
     yrmax = pd.Timestamp(time.max()).year
     wy_list = []
     wy_list = [yr+1]
-    #while yr<=yrmax:
-    #    if (np.datetime64('%d-10-01' % yr) >= time[0]) and (sum(time>=np.datetime64('%d-10-01' % yr))>0):
-    #        wy_list.append(yr+1)
-    #    yr += 1
+    while yr<=yrmax:
+        if (np.datetime64('%d-10-01' % yr) >= time[0]) and (sum(time>=np.datetime64('%d-10-01' % yr))>0):
+            wy_list.append(yr+1)
+        yr += 1
 
     # time step in days
     deltat_days = (time[1]-time[0])/np.timedelta64(1,'D')
@@ -510,9 +510,6 @@ for balance_table_fn in table_list:
 
             # initialize dataframe
             df_tavg = pd.DataFrame()
-
-#            if tavg=='Seasonal':
-#                sys.exit()
 
             # loop through the groups
             for group in group_list: